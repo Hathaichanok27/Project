d55761c709a1275b8acd8eefc8ad054272c56b92
Hathaichanok27 <60160008@go.buu.ac.th>
2021-01-27 14:49:02 +0700

    Add roommedia page

--- app/routes/web.php
+++ app/routes/web.php
@@ -24,3 +24,4 @@ Route::get('admin/home', 'HomeController@adminhome')->name('admin.home')->middle
 Route::get('superadmin/home', 'HomeController@superadminHome')->name('superadmin.home')->middleware('is_superadmin');
 
 Route::resource('roombookings', RoombookingController::class);
+Route::resource('roommedias', RoommediaController::class);
