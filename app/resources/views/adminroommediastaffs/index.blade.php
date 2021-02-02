ff543fb9dc19693f83283638943b20a046436eac
Hathaichanok27 <60160008@go.buu.ac.th>
2021-02-02 18:49:16 +0700

    Add admin page

---
+++ app/resources/views/adminroommediastaffs/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.adminlayout')
+
+@section('content')
+	<div class="page-container">
+		<div class="container">
+			<div class="row justify-content-center">
+            <h3 class="text-light heading-divided"><i class="fas fa-map-marker-alt"></i> โซนชั้น 6 <small>6<sup>th</sup> floor</small> (สำหรับเจ้าหน้าที่)</h3>
+                <div class="col-md-4 col-md-offset-2">
+                    <div class="panel border-grey">
+                        <div class="panel-body text-center">
+                            <div class="icon-object border-danger text-danger">
+                                <i class="fas fa-users"></i>
+                            </div>
+                            <h5 class="text-semibold">ห้องสื่อศึกษากลุ่ม</h5>
+                            <p class="mb-15">ห้องศึกษากลุ่มมัลติมีเดีย ให้บริการรับชมสื่อมัลติมีเดีย<br>เป็นเวลา 3 ชม. กำหนดผู้ใช้ขั้นต่ำ 3 คน</p>
+                            <a href="{{ route('queuelistmediagroups.index') }}"" class="btn bg-danger-400">รายการการจอง <i class="fas fa-search"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel border-grey">
+                        <div class="panel-body text-center">
+                            <div class="icon-object border-success text-success">
+                                <i class="fas fa-user"></i>
+                            </div>
+                            <h5 class="text-semibold">ห้องสื่อศึกษาเดี่ยว</h5>
+                            <p class="mb-15">ห้องศึกษาเดี่ยวให้บริการเครื่องสำหรับรับชมสื่อมัลติเมียเดีย<br>เป็นเวลา 3 ชม. กำหนดผู้ใช้ 1-2 คน</p>
+                            <a href="{{ route('queuelistmediasingles.index') }}"" class="btn bg-success-400">รายการการจอง <i class="fas fa-search"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>     
+    </div>
+@endsection
