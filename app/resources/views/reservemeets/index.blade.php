0af1258d7dd063c64b79dd5af337930ad9971f42
Hathaichanok27 <60160008@go.buu.ac.th>
2021-02-09 22:57:08 +0700

    Update reservemeets/index.blade.php

--- app/resources/views/reservemeets/index.blade.php
+++ app/resources/views/reservemeets/index.blade.php
@@ -7,105 +7,117 @@
                 <h4><i class="fas fa-search"></i> จองห้องประชุม </h4>
                 <ul class="breadcrumb breadcrumb-caret position-right">
                     <li><a href="{{ route('roombookings.index') }}">หน้าแรก</a></li>
-                    <li><a href="{{ route('roommedias.index') }}">จองห้องประชุม</a></li>
+                    <li><a href="{{ route('roommeetings.index') }}">ห้องประชุม</a></li>
+                    <li class="active">จองห้องประชุม</a></li>
                 </ul>   
             </div>
-
-            <div id="div_name">
-                <label for="name">Description</label>
-                <input type="text" pattern="\s*\S+.*" id="name" name="name" required="" aria-required="true" maxlength="80" value="">
+            <div class="form-group">
+                <label class="col-sm-1 control-label">Description: </label>
+                <div class="col-sm-6">
+                    <input class="form-control" id="description" type="text" placeholder="Description">
+                </div>
             </div>
             <br>
-            <form action="">
-                <label for="date_start">Start: </label>
-                <input type="date" id="date_start" name="date_start">
-                <select id="start_seconds" name="start_seconds" style="width: 144.8px;">
-                <option value="28800">08:00</option>
-                <option value="30600">08:30</option>
-                <option value="32400">09:00</option>
-                <option value="34200">09:30</option>
-                <option value="36000">10:00</option>
-                <option value="37800">10:30</option>
-                <option value="39600">11:00</option>
-                <option value="41400">11:30</option>
-                <option value="43200">12:00</option>
-                <option value="45000">12:30</option>
-                <option value="46800">13:00</option>
-                <option value="48600">13:30</option>
-                <option value="50400">14:00</option>
-                <option value="52200">14:30</option>
-                <option value="54000">15:00</option>
-                <option value="55800">15:30</option>
-                <option value="57600">16:00</option>
-                <option value="59400">16:30</option>
-                <option value="61200">17:00</option>
-                <option value="63000">17:30</option>
-                <option value="64800">18:00</option>
-                <option value="66600">18:30</option>
-                <option value="68400">19:00</option>
-                <option value="70200">19:30</option>
-                <option value="72000">20:00</option>
-                <option value="73800">20:30</option>
-            </select>
-            </form>  
-            
-            <br>
-            <form action="">
-                <label for="date_start">End: </label>
-                <input type="date" id="date_start" name="date_start">
-                <select id="start_seconds" name="start_seconds" style="width: 144.8px;">
-                <option value="28800">08:00</option>
-                <option value="30600">08:30</option>
-                <option value="32400">09:00</option>
-                <option value="34200">09:30</option>
-                <option value="36000">10:00</option>
-                <option value="37800">10:30</option>
-                <option value="39600">11:00</option>
-                <option value="41400">11:30</option>
-                <option value="43200">12:00</option>
-                <option value="45000">12:30</option>
-                <option value="46800">13:00</option>
-                <option value="48600">13:30</option>
-                <option value="50400">14:00</option>
-                <option value="52200">14:30</option>
-                <option value="54000">15:00</option>
-                <option value="55800">15:30</option>
-                <option value="57600">16:00</option>
-                <option value="59400">16:30</option>
-                <option value="61200">17:00</option>
-                <option value="63000">17:30</option>
-                <option value="64800">18:00</option>
-                <option value="66600">18:30</option>
-                <option value="68400">19:00</option>
-                <option value="70200">19:30</option>
-                <option value="72000">20:00</option>
-                <option value="73800">20:30</option>
-            </select> 
-            </form> 
-            
-            <br>
-            <div id="div_areas" style="">
-                <label for="area">Area:</label>
-                <select id="area" name="area">
-                <option value="8">พื้นที่ชั้น 3 (3rd Floor)</option>
-                <option value="9" selected="selected">พื้นที่ชั้น 4 (4th Floor)</option>
-                <option value="10">พื้นที่ชั้น 5 (5th Floor)</option>
-                <option value="11">พื้นที่ชั้น 5 เฉพาะอาจารย์</option>
-                <option value="12">พื้นที่ชั้น 6 (6th Floor)</option>
-                <option value="13">Mini Home Theatre (6th Floor)</option>
-                <option value="14">พื้นที่ชั้น 6 คาราโอเกะ</option>
-                </select>
+            <div class="form-group">
+                <label class="col-sm-1 control-label" for="date_start">Start: </label>
+                <div class="col-sm-3">
+                    <input type="date" class="form-control" id="date_start" name="date_start">
+                </div>
+                <div class="col-sm-3">
+                    <select class="form-control" id="time_start" name="time_start">
+                        <option value="08.00">08:00</option>
+                        <option value="08.30">08:30</option>
+                        <option value="09.00">09:00</option>
+                        <option value="09.30">09:30</option>
+                        <option value="10.00">10:00</option>
+                        <option value="10.30">10:30</option>
+                        <option value="11.00">11:00</option>
+                        <option value="11.30">11:30</option>
+                        <option value="12.00">12:00</option>
+                        <option value="12.30">12:30</option>
+                        <option value="13.00">13:00</option>
+                        <option value="13.30">13:30</option>
+                        <option value="14.00">14:00</option>
+                        <option value="14.30">14:30</option>
+                        <option value="15.00">15:00</option>
+                        <option value="15.30">15:30</option>
+                        <option value="16.00">16:00</option>
+                        <option value="16.30">16:30</option>
+                        <option value="17.00">17:00</option>
+                        <option value="17.30">17:30</option>
+                        <option value="18.00">18:00</option>
+                        <option value="18.30">18:30</option>
+                        <option value="19.00">19:00</option>
+                        <option value="19.30">19:30</option>
+                        <option value="20.00">20:00</option>
+                    </select>
+                </div>
             </div>
             <br>
-            <div id="div_areas" style="">
-                <label for="area">Rooms:</label>
-                <select id="area" name="area">
-                <option value="8">room 1</option>
-                <option value="9">room 2 </option>
-                <option value="10">room 3</option>
-                <option value="11">room 4</option>
-                <option value="12">room 5</option>
-                </select>
+            <div class="form-group">
+                <label class="col-sm-1 control-label" for="date_start">End: </label>
+                <div class="col-sm-3">
+                    <input type="date" class="form-control" id="date_end" name="date_end">
+                </div>
+                <div class="col-sm-3">
+                    <select class="form-control" id="time_end" name="time_end">
+                        <option value="08.00">08:00</option>
+                        <option value="08.30">08:30</option>
+                        <option value="09.00">09:00</option>
+                        <option value="09.30">09:30</option>
+                        <option value="10.00">10:00</option>
+                        <option value="10.30">10:30</option>
+                        <option value="11.00">11:00</option>
+                        <option value="11.30">11:30</option>
+                        <option value="12.00">12:00</option>
+                        <option value="12.30">12:30</option>
+                        <option value="13.00">13:00</option>
+                        <option value="13.30">13:30</option>
+                        <option value="14.00">14:00</option>
+                        <option value="14.30">14:30</option>
+                        <option value="15.00">15:00</option>
+                        <option value="15.30">15:30</option>
+                        <option value="16.00">16:00</option>
+                        <option value="16.30">16:30</option>
+                        <option value="17.00">17:00</option>
+                        <option value="17.30">17:30</option>
+                        <option value="18.00">18:00</option>
+                        <option value="18.30">18:30</option>
+                        <option value="19.00">19:00</option>
+                        <option value="19.30">19:30</option>
+                        <option value="20.00">20:00</option>
+                    </select>
+                </div>
+            </div> 
+            <br>
+            <div class="form-group">
+                <label class="col-sm-1 control-label" for="area">Area: </label>
+                <div class="col-sm-6">
+                    <select class="form-control" id="area" name="area">
+                        <option value="floor-3">ชั้น 3 (3rd Floor)</option>
+                        <option value="floor-4">ชั้น 4 (4th Floor)</option>
+                        <option value="floor-5">ชั้น 5 (5th Floor)</option>
+                        <option value="floor-5-aj">ชั้น 5 - เฉพาะอาจารย์</option>
+                        <option value="floor-6-mini-home-theatre">ชั้น 6 - Mini Home Theatre</option>
+                        <option value="floor-6-karaoke">ชั้น 6 - Karaoke</option>
+                    </select>
+                </div>
             </div>
-            <input type="submit">
+            <br>
+            <div class="form-group">
+                <label class="col-sm-1 control-label" for="room">Room: </label>
+                <div class="col-sm-6">
+                    <select class="form-control" id="room" name="room">
+                        <option value="room-1">room 1</option>
+                        <option value="room-2">room 2</option>
+                        <option value="room-3">room 3</option>
+                        <option value="room-4">room 4</option>
+                        <option value="room-5">room 5</option>
+                    </select>
+                </div>
+            </div>    
+            <br>  
+            <input class="btn btn-primary" type="submit" value="Submit">
+        </div>
+    </div>
 @endsection 
