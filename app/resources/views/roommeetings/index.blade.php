a3f59cf760d31cd86d78dbfb7caf154d605681ad
Hathaichanok27 <60160008@go.buu.ac.th>
2021-02-10 11:28:14 +0700

    Edit roommeetings

--- app/resources/views/roommeetings/index.blade.php
+++ app/resources/views/roommeetings/index.blade.php
@@ -19,16 +19,17 @@
                 </div>
                 <div class="form-group col-md-6">
                     <label for="select-room">Room</label>
-                    <select id="floor-3" class="form-control">
-                        <option value="floor-3">ชั้น 3 (3rd Floor)</option>
-                        <option value="floor-4">ชั้น 4 (4th Floor)</option>
-                        <option value="floor-5">ชั้น 5 (5th Floor)</option>
-                        <option value="floor-5-aj">ชั้น 5 - เฉพาะอาจารย์</option>
-                        <option value="floor-6-mini-home-theatre">ชั้น 6 - Mini Home Theatre</option>
-                        <option value="floor-6-karaoke">ชั้น 6 - Karaoke</option>
+                    <select id="select-room" class="form-control">
+                        <option value="room-1">Room 1</option>
+                        <option value="room-2">Room 2</option>
+                        <option value="room-3">Room 3</option>
+                        <option value="room-4">Room 4</option>
+                        <option value="room-5">Room 5</option>
+                        <option value="room-6">Room 6</option>
                     </select>
                 </div>
             </div>
+            
         </div>     
     </div>
 @endsection
