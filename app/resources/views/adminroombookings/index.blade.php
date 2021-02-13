dd51c33982be3b827ff20a9b478c54c526ef2630
Hathaichanok27 <60160008@go.buu.ac.th>
2021-02-13 15:07:34 +0700

    Edit roombooking

--- app/resources/views/adminroombookings/index.blade.php
+++ app/resources/views/adminroombookings/index.blade.php
@@ -8,12 +8,12 @@
                 <div class="col-md-4 col-md-offset-2">
                     <div class="panel border-grey">
                         <div class="panel-body text-center">
-                            <div class="icon-object border-warning text-warning">
+                            <div class="icon-object border-success text-success">
                                 <i class="fas fa-users"></i>
                             </div>
                             <h5 class="text-semibold">ห้องประชุม</h5>
                             <p class="mb-15">ให้บริการห้องประชุม<br>เป็นเวลา 2 ชม.</p>
-                            <a href="" class="btn bg-warning-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
+                            <a href="" class="btn bg-success-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
                         </div>
                     </div>
                 </div>
@@ -21,12 +21,12 @@
                 <div class="col-md-4">
                     <div class="panel border-grey">
                         <div class="panel-body text-center">
-                            <div class="icon-object border-primary text-primary">
+                            <div class="icon-object border-danger text-danger">
                                 <i class="fas fa-play"></i>
                             </div>
                             <h5 class="text-semibold">ห้องสื่อศึกษา</h5>
                             <p class="mb-15">ให้บริการเครื่องรับชมสื่อมัลติเมียเดีย<br>เป็นเวลา 3 ชม.</p>
-                            <a href="{{ route('adminroommedias.index') }}" class="btn bg-primary-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
+                            <a href="{{ route('adminroommedias.index') }}" class="btn bg-danger-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
                         </div>
                     </div>
                 </div>
