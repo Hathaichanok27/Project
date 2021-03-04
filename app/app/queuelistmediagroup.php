18a88a7fc4407b00e3c6420904b6ccb24bbd5b45
Hathaichanok27 <60160008@go.buu.ac.th>
2021-03-04 20:06:16 +0700

    add model queuelistmediagroup

---
+++ app/app/queuelistmediagroup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class queuelistmediagroup extends Model
+{
+    protected $fillable = [
+        'user_fullname', 'username', 'user_telnum', 'user_email',
+        'room_type', 'room_floor', 'room_name', 'book_createtime', 'book_starttime', 'book_endtime', 'book_status'
+
+    ];
+}
