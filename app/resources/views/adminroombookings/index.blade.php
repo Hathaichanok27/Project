f2b8b303cec234f02adebc6307da79e33c438889
Hathaichanok27 <60160008@go.buu.ac.th>
2021-03-19 00:53:04 +0700

    Edit admin and superadmin home

--- app/resources/views/adminroombookings/index.blade.php
+++ app/resources/views/adminroombookings/index.blade.php
@@ -12,7 +12,7 @@
                                 <i class="fas fa-users"></i>
                             </div>
                             <h5 class="text-semibold">ห้องประชุม</h5>
-                            <p class="mb-15">ให้บริการห้องประชุม<br>เป็นเวลา 2 ชม.</p>
+                            <p class="mb-15">สำหรับเจ้าหน้าที่ จัดการห้องประชุม</p>
                             <a href="" class="btn bg-success-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
                         </div>
                     </div>
@@ -25,8 +25,8 @@
                                 <i class="fas fa-play"></i>
                             </div>
                             <h5 class="text-semibold">ห้องสื่อศึกษา</h5>
-                            <p class="mb-15">ให้บริการเครื่องรับชมสื่อมัลติเมียเดีย<br>เป็นเวลา 3 ชม.</p>
-                            <a href="{{ route('adminroommedias.index') }}" class="btn bg-danger-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
+                            <p class="mb-15">สำหรับเจ้าหน้าที่ จัดการห้องสื่อศึกษา</p>
+                            <a href="{{ route('adminroommediastaffs.index') }}" class="btn bg-danger-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
                         </div>
                     </div>
                 </div>
--- app/resources/views/superadminroombookings/index.blade.php
+++ app/resources/views/superadminroombookings/index.blade.php
@@ -8,12 +8,11 @@
                 <div class="col-md-4 col-md-offset-2">
                     <div class="panel border-grey">
                         <div class="panel-body text-center">
-                            <div class="icon-object border-success text-success">
-                                <i class="fas fa-users"></i>
+                            <div class="icon-object border-primary text-primary">
+                                <i class="fas fa-fas fa-wrench"></i>
                             </div>
-                            <h5 class="text-semibold">ห้องประชุม</h5>
-                            <p class="mb-15">ให้บริการห้องประชุม<br>เป็นเวลา 2 ชม.</p>
-                            <a href="" class="btn bg-success-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
+                            <h5 class="text-semibold">จัดการเจ้าหน้าที่</h5>
+                            <a href="{{ route('manageadmins.index') }}" class="btn bg-primary-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
                         </div>
                     </div>
                 </div>
@@ -22,11 +21,10 @@
                     <div class="panel border-grey">
                         <div class="panel-body text-center">
                             <div class="icon-object border-danger text-danger">
-                                <i class="fas fa-play"></i>
+                                <i class="fas fa-bars"></i>
                             </div>
-                            <h5 class="text-semibold">ห้องสื่อศึกษา</h5>
-                            <p class="mb-15">ให้บริการเครื่องรับชมสื่อมัลติเมียเดีย<br>เป็นเวลา 3 ชม.</p>
-                            <a href="{{ route('superadminroommedias.index') }}" class="btn bg-danger-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
+                            <h5 class="text-semibold">จัดการห้อง</h5>
+                            <a href="{{ route('rooms.index') }}" class="btn bg-danger-400">ไปที่ <i class="fas fa-angle-double-right"></i></a>
                         </div>
                     </div>
                 </div>
