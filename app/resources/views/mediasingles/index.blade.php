5560e653dab669f91d071e82157a6f4bc219f396
Hathaichanok27 <60160008@go.buu.ac.th>
2021-03-22 13:31:43 +0700

    Edit status media

--- app/resources/views/mediasingles/index.blade.php
+++ app/resources/views/mediasingles/index.blade.php
@@ -30,7 +30,7 @@
                                     <span class="text-muted countdown" data-endtime="2021-01-01 09:00:00">00 : 00 : 00</span>
                                 </div>
                                 <div class="media-right media-middle">
-                                    <span class="label label-lg label-<?php echo $room->room_status == 'ไม่เปิดใช้งาน'?'danger':''?><?php echo $room->room_status == '1'?'success':''?><?php echo $room->room_status == '2'?'warning':''?>"> {{ $room->room_status }}</span>
+                                    <span class="label label-lg label-<?php echo $room->room_status == '0'?'danger':''?><?php echo $room->room_status == '1'?'success':''?><?php echo $room->room_status == '2'?'warning':''?>"> {{ $room->room_status }}</span>
                                 </div>
                             </div>
                         </div>
