8efad332be35df05c42df08755e2227ce8f7c3b2
Hathaichanok27 <60160008@go.buu.ac.th>
2021-03-24 00:00:47 +0700

    Edit model

--- app/app/Mybooking.php
+++ app/app/Mybooking.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Mybooking extends Model
 {
     protected $fillable = [
-        'room_type', 'room_floor', 'room_name', 'book_createtime', 'book_starttime', 'book_endtime', 'username', 'book_status'
+        'username',
+        'room_type', 'room_floor', 'room_name', 'book_createtime', 'book_starttime', 'book_endtime', 'book_status'
     ];
 }
