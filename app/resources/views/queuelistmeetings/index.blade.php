422c420f780f1dc0ea539cb9c2e725c5f214ea71
Hathaichanok27 <60160008@go.buu.ac.th>
2021-03-24 00:11:28 +0700

    Edit roommeeting #3

--- app/resources/views/queuelistmeetings/index.blade.php
+++ app/resources/views/queuelistmeetings/index.blade.php
@@ -51,7 +51,6 @@
                                                 <td>{{ $reservemeet->user_fullname }}</td>
                                                 <td>{{ $reservemeet->time_start }}</td>
                                                 <td>{{ $reservemeet->time_end }}</td>
-                                                <!-- 0 รออนุมัติ, 1 อนุมัติ, 2 ยกเลิกการจอง, 3 คืนห้อง -->
                                                 <td class="text-center"><b style="color:<?php echo $reservemeet->book_status == 'รอการอนุมัติ'?'#f0ad4e':''?><?php echo $reservemeet->book_status == 'อนุมัติ'?'#5cb85c':''?><?php echo $reservemeet->book_status == 'ยกเลิกการจอง'?'#d9534f':''?><?php echo $reservemeet->book_status == 'คืนห้อง'?'#0275d8':''?>">{{$reservemeet->book_status}}</b></td>
                                                 <td class="text-center">
                                                     <ul class="icons-list">
@@ -62,7 +61,9 @@
                                                                 <li class="divider"></li>
                                                                 <li><a href="" data-toggle="modal" data-target="#myModal2_{{ $i }}" style="color:#26A65B;"><i class="fas fa-sign-in-alt"></i> เข้าห้อง</a></li>                                                                
                                                                 <li class="divider"></li>
-                                                                <li><a href="" data-toggle="modal" data-target="#myModal3_{{ $i }}" style="color:#e74c3c;"><i class="fas fa-times"></i> ยกเลิกการจอง</a></li>                                                                
+                                                                <li><a href="" data-toggle="modal" data-target="#myModal3_{{ $i }}" style="color:#0275d8;"><i class="fas fa-sign-out-alt"></i> คืนห้อง</a></li>
+                                                                <li class="divider"></li>
+                                                                <li><a href="" data-toggle="modal" data-target="#myModal4_{{ $i }}" style="color:#e74c3c;"><i class="fas fa-times"></i> ยกเลิกการจอง</a></li>                                                                 
                                                             </ul>
                                                         </li>
                                                     </ul>
@@ -170,7 +171,35 @@
                                 </div>
                                 <!-- Modal3 -->
                                 <div class="modal" id="myModal3_<?php echo $o?>" role="dialog">
-                                    <div class="sweet-alert showSweetAlert visible" data-custom-class="" data-has-cancel-button="true" data-has-confirm-button="true" data-allow-outside-click="false" data-has-done-function="true" data-animation="pop" data-timer="null" style="display: block; margin-top: -145px;">
+                                    <div class="sweet-alert showSweetAlert visible" data-animation="pop" style="display: block; margin-top: -145px;">
+                                        <div class="sa-icon sa-error" style="display: none;">
+                                            <span class="sa-x-mark">
+                                                <span class="sa-line sa-left"></span>
+                                                <span class="sa-line sa-right"></span>
+                                            </span>
+                                        </div>
+                                        <div class="sa-icon sa-warning pulseWarning" style="display: block;">
+                                            <span class="sa-body pulseWarningIns"></span>
+                                            <span class="sa-dot pulseWarningIns"></span>
+                                        </div>
+                                        <form action="{{ route('queuelistmeetings.update',$reservemeet->id) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <div class="sa-icon sa-custom" style="display: none;"></div>
+                                            <h2>Confirmation</h2>
+                                            <p>คืนห้องของ {{ $reservemeet->user_fullname }} ?</p>
+                                            <div class="sa-button-container">
+                                                <input type="hidden" name="time_start" value="{{ $reservemeet->time_start }}">
+                                                <input type="hidden" name="time_end" value="{{ $reservemeet->time_end }}">
+                                                <input type="hidden" name="book_status" value="คืนห้อง"><button type="button" class="cancel" data-dismiss="modal">ไม่ใช่</button>
+                                                <button type="submit" class="btn btn-danger">ใช่, คืนห้อง</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                <!-- Modal4 -->
+                                <div class="modal" id="myModal4_<?php echo $o?>" role="dialog">
+                                    <div class="sweet-alert showSweetAlert visible" data-animation="pop" style="display: block; margin-top: -145px;">
                                         <div class="sa-icon sa-error" style="display: none;">
                                             <span class="sa-x-mark">
                                                 <span class="sa-line sa-left"></span>
