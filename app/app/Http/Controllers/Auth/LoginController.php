16feb1d55cdafb1ee63014270f2a0496c55353f8
60160039 <60160039@go.buu.ac.th>
2021-03-28 10:03:40 +0700

    Update login Ldab

--- app/app/Http/Controllers/Auth/LoginController.php
+++ app/app/Http/Controllers/Auth/LoginController.php
@@ -1,11 +1,11 @@
-<?php
-  
+<?php 
+
 namespace App\Http\Controllers\Auth;
    
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-   
+use App\User;  
 class LoginController extends Controller
 {
     /*
@@ -41,13 +41,20 @@ class LoginController extends Controller
     public function login(Request $request)
     {   
         $input = $request->all();
-   
+        print(phpversion());
         $this->validate($request, [
             'username' => 'required',
             'password' => 'required',
         ]);
-   
-        if(auth()->attempt(array('username' => $input['username'], 'password' => $input['password'])))
+        $login = $input['username'];
+        $password = $input['password'];
+         // check LDAP
+      /*  $result = $this->check_with_ad ($login, $password);
+          print_r($result);
+        if ($result <> "notfound")
+         { echo text;
+         }else */
+         if(auth()->attempt(array('username' => $input['username'], 'password' => $input['password'])))
         {
             if (auth()->user()->is_admin == 1) {
                 return redirect()->route('admin.home');
@@ -62,4 +69,32 @@ class LoginController extends Controller
         }
           
     }
+   /* function check_with_ad ($user, $key) 
+	{
+		define ("AD_ENABLED", 1, true);
+		define ("AD_SERVER", "10.4.1.82", true);
+		define ("AD_BASEDN", "ou=People,dc=buu,dc=ac,dc=th", true);
+		define ("AD_FILTER", "(cn=XUID)", true);	
+	
+		$retval = "notfound"; 
+		$vlan_no = 1; 
+		$network_id = 0; 
+		$ad = @ldap_connect("ldap://" . AD_SERVER);
+		if ($ad && $user != "" && $key != "")
+		{
+			ldap_set_option($ad, LDAP_OPT_PROTOCOL_VERSION, 3);
+			ldap_set_option($ad, LDAP_OPT_REFERRALS, 0);
+			if (@ldap_bind($ad,"$user@buu.ac.th","$key")) 
+			{
+				$filter = preg_replace("/XUID/", "$user", AD_FILTER);
+				$result = ldap_search($ad, AD_BASEDN, $filter);
+				$entries = ldap_get_entries($ad, $result);
+				$retval = trim($entries[0]["givenname"][0]." ".$entries[0]["sn"][0]);
+			} 
+			ldap_unbind($ad); 
+		}
+		return $retval;
+	}*/
+
+
 }
