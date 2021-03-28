9cb2b791130be96c4d44a5effd44e7e98ffe5d43
Hathaichanok27 <60160008@go.buu.ac.th>
2021-03-28 21:28:14 +0700

    Edit roommeeting #7

--- app/app/Http/Controllers/QueuelistmeetingController.php
+++ app/app/Http/Controllers/QueuelistmeetingController.php
@@ -4,23 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Reservemeet;
-
+use App\Room;
 
 class QueuelistmeetingController extends Controller
 {
     public function index()
     {
-        $reservemeets = Reservemeet::paginate();
-        
-        return view('queuelistmeetings.index',compact('reservemeets'))
-            ->with('i', (request()->input('page', 1) - 1) * 5);
+        $reservemeets1 = Reservemeet::select("*")->where("book_status", "=", "รอการอนุมัติ")->get();
+        $reservemeets2 = Reservemeet::select("*")->where("book_status", "=", "อนุมัติ")->get();
+        $reservemeets3 = Reservemeet::select("*")->where("book_status", "=", "คืนห้อง")->get();
+        $reservemeets4 = Reservemeet::select("*")->where("book_status", "=", "ยกเลิกการจอง")->get();
+        $count1 = count($reservemeets1);
+        $count2 = count($reservemeets2);
+        $count3 = count($reservemeets3);
+        $count4 = count($reservemeets4);
+        // $rooms = Room::paginate();
+
+        return view('queuelistmeetings.index',compact(['reservemeets1','reservemeets2','reservemeets3','reservemeets4',
+                                                            'count1','count2','count3','count4']))
+                ->with('i', (request()->input('page', 1) - 1) * 5); 
     }
 
     public function update(Request $request, $id)
     {
         $where = array('id' => $id);
-        $updateArr = ['book_status' => $request->input('book_status'),];
+        // $where1 = array('room_name' => $request->input('room_name'));
+        $updateArr = [
+                       'book_status' => $request->input('book_status')
+                     ];
+        // $updateArr1 = [
+        //                 'room_status'  => $request->input('room_status'),
+        //              ];
         $booking  = Reservemeet::where($where)->update($updateArr);
+        // $booking1  = Room::where($where1)->update($updateArr1);
         
         return redirect()->route('queuelistmeetings.index');
     }
